29a0e8bc0a3567eb97ea0433d0e0c1c6e8d94f32
Jacob Hein <heinj82@uwosh.edu>
2020-02-27 16:00:22 -0600

    Everything is aligned and ready for submission

--- a3/a3/a3.js
+++ a3/a3/a3.js
@@ -1,15 +1,15 @@
 /********************************************
-   CS 331 - A3
+CS 331 - A3
 
-   Your name(s):  Jacob Hein
+Your name(s):  Jacob Hein
 
-                  Dan Kuphal
+Dan Kuphal
 
- ********************************************/
+********************************************/
 
 var fp = require('./fp');
 if ( ! exports ) {
-   var exports = [ ];
+    var exports = [ ];
 }
 
 var removeLast = function (ns) {
@@ -38,38 +38,19 @@ var closest = function (ns, n) {
 
     if (fp.isNull(fp.tl(ns))) {
         return fp.hd(ns);
-    } else if (fp.isEq(fp.hd(ns), n)) {
-        return fp.hd(ns);
-    }
-    else {
-        if (fp.isLT(fp.hd(ns), n)) { // is first element < n
-            if (fp.isLT(fp.hd(fp.tl(ns)), n)) { // both < n
-                if (fp.isLT(fp.sub(n, fp.hd(ns)), fp.sub(n, fp.hd(fp.tl(ns))))) { // first element is closer to n
-                    return closest(fp.cons(fp.hd(ns), fp.tl(fp.tl(ns))), n); // makes list with head - 2nd element + rest.
-                } else { // second element is closer to n
-                    return closest(fp.tl(ns), n);
-                }
-            } else { //fp.isGT(fp.hd(fp.tl(ns), n))  // hd < n , hd.tl > n
-                if (fp.isLT(fp.sub(n, fp.hd(ns)), fp.sub(fp.hd(fp.tl(ns)), n))) { // first element is closer to n
-                    return closest(fp.cons(fp.hd(ns), fp.tl(fp.tl(ns))), n); // makes list with head - 2nd element + rest.
-                } else { // second element is closer to n
-                    return closest(fp.tl(ns), n); // makes list with head - 2nd element + rest.
-                }
-            }
-        } else { // is first element > n
-            if (fp.isLT(fp.hd(fp.tl(ns)), n)) { // hd > n , hd.tl < n
-                if (fp.isLT(fp.sub(fp.hd(ns), n), fp.sub(n, fp.hd(fp.tl(ns))))) { // first element is closer to n
-                    return closest(fp.cons(fp.hd(ns), fp.tl(fp.tl(ns))), n); // makes list with head - 2nd element + rest.
-                } else { // second element is close to n
-                    return closest(fp.tl(ns), n); // makes list with head - 2nd element + rest.
-                }
-            } else { //fp.isGT(fp.hd(fp.tl(ns), n)) // both > n
-                if (fp.isLT(fp.sub(fp.hd(ns), n), fp.sub(fp.hd(fp.tl(ns)), n))) { // first element is closer to n
-                    return closest(fp.cons(fp.hd(ns), fp.tl(fp.tl(ns))), n); // makes list with head - 2nd element + rest.
-                } else { // second element is close to n
-                    return closest(fp.tl(ns), n); // makes list with head - 2nd element + rest.
-                }
-            }
+    } else {
+        if (fp.isGT(fp.mul(fp.sub(n, fp.hd(ns)),
+            fp.sub(n, fp.hd(ns))),
+            fp.mul(fp.sub(n, fp.hd(fp.tl(ns))),
+            fp.sub(n, fp.hd(fp.tl(ns)))))) {
+            return closest(fp.tl(ns), n);
+        } else if (fp.isEq(fp.mul(fp.sub(n, fp.hd(ns)),
+                   fp.sub(n, fp.hd(ns))),
+                   fp.mul(fp.sub(n, fp.hd(fp.tl(ns))),
+                   fp.sub(n, fp.hd(fp.tl(ns)))))) {
+            return closest(fp.tl(ns), n);
+        } else {
+            return closest(fp.cons(fp.hd(ns), fp.tl(fp.tl(ns))), n);
         }
     }
 
@@ -84,14 +65,15 @@ var splitList = function (ns) {
     } else if (fp.isGT(fp.hd(ns), fp.hd(fp.tl(ns)))) {
         return fp.cons(fp.cons(fp.hd(ns), []), splitList(fp.tl(ns)));
     } else {
-         return fp.cons(fp.cons(fp.hd(ns), fp.hd(splitList(fp.tl(ns)))), fp.tl(splitList(fp.tl(ns))));
+        return fp.cons(fp.cons(fp.hd(ns), fp.hd(splitList(fp.tl(ns)))),
+               fp.tl(splitList(fp.tl(ns))));
     }
 
 };
 
 var addDigits = function (n) {
 
-    if (fp.isLT(n, 10)){
+    if (fp.isLT(n, 10)) {
         return n;
     } else {
         return fp.add(fp.rem(n,10), addDigits(fp.div(n, 10)));
@@ -101,7 +83,7 @@ var addDigits = function (n) {
 
 var persistence = function (n) {
 
-    if (fp.isLT(n, 10)){
+    if (fp.isLT(n, 10)) {
         return 0;
     } else {
         return fp.add(1, persistence(addDigits(n)));
