baad6338f7b1db17a14deefec66f39b08faf10bb
Dan Kuphal <djkdaboss@gmail.com>
2020-05-10 14:20:23 -0500

    fix to test suite

--- a7b/a7b/test.js
+++ a7b/a7b/test.js
@@ -3,8 +3,8 @@ var is = require('./is');
 
 console.log(is.take(a7b.makeSqrtSeq(2,1),5));
 console.log(is.take(a7b.makeSqrtSeq(10000,1),10));
-console.log(sqrt(makeSqrtSeq(2,1),1));
-console.log(sqrt(makeSqrtSeq(2,1),0.1));
-console.log(sqrt(makeSqrtSeq(2,1),1e-2));
-console.log(is.take(cumulate(is.from(1)),10));
-console.log(is.take(cumulate(is.iterates(function(n){return n * 2;}, 1)), 10));
+console.log(a7b.sqrt(a7b.makeSqrtSeq(2,1),1));
+console.log(a7b.sqrt(a7b.makeSqrtSeq(2,1),0.1));
+console.log(a7b.sqrt(a7b.makeSqrtSeq(2,1),1e-2));
+console.log(is.take(a7b.cumulate(is.from(1)),10));
+console.log(is.take(a7b.cumulate(is.iterates(function(n){return n * 2;}, 1)), 10));
