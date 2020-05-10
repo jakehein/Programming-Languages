9fab8e4ddc5dac2c9bf66f9f5668f6e29e4a1c60
Jacob Hein <heinj82@uwosh.edu>
2020-05-10 13:10:16 -0500

    initial stuff

---
+++ a7b/a7b/a7b.js
@@ -0,0 +1,43 @@
+/*************************************************************
+ * CS 331 - Assignment A7b
+ * Student name(s) : <Your name goes here>
+ *                   <Your name goes here>
+ *************************************************************/
+
+var is = require('./is');
+
+module.exports = { };
+
+"use strict";
+
+(function (exports) {
+
+    // Do NOT modify this file above this point
+    
+    ///////////////////////////////////////////////////////////////////////////
+    //                              Problem 1
+    ///////////////////////////////////////////////////////////////////////////
+
+    var makeSqrtSeq = function () { return 1; }; // To be modified
+
+    ///////////////////////////////////////////////////////////////////////////
+    //                              Problem 2
+    ///////////////////////////////////////////////////////////////////////////
+
+    var sqrt = function () { return 1; }; // To be modified
+    
+    ///////////////////////////////////////////////////////////////////////////
+    //                              Problem 3
+    ///////////////////////////////////////////////////////////////////////////
+
+    var cumulate = function () { return 1; }; // To be modified
+	
+    // Do NOT modify this file below this point
+    
+    exports.makeSqrtSeq = makeSqrtSeq;
+    exports.sqrt = sqrt;    
+    exports.cumulate = cumulate;
+    
+}) (module.exports);
+
+
