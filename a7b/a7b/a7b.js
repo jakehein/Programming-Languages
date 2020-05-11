9c9a8b78d24a7d7c3343f9be627494cc83dff2b9
Jacob Hein <heinj82@uwosh.edu>
2020-05-11 09:50:50 -0500

    beautified and tabs converted to spaces

--- a7b/a7b/a7b.js
+++ a7b/a7b/a7b.js
@@ -6,33 +6,36 @@
 
 var is = require('./is');
 
-module.exports = { };
+module.exports = {};
 
 "use strict";
 
-(function (exports) {
+(function(exports) {
 
     // Do NOT modify this file above this point
 
     ///////////////////////////////////////////////////////////////////////////
     //                              Problem 1
     ///////////////////////////////////////////////////////////////////////////
-    var makeSqrtSeq = function (num, guess) {
-        return is.iterates(function(guess) {return (guess + num / guess) / 2; },
-        guess);
-};
+    var makeSqrtSeq = function(num, guess) {
+        return is.iterates(function(guess) {
+                return (guess + num / guess) / 2;
+            },
+            guess);
+    };
 
     ///////////////////////////////////////////////////////////////////////////
     //                              Problem 2
     ///////////////////////////////////////////////////////////////////////////
 
-    var sqrt = function (seq, eps) {
+    var sqrt = function(seq, eps) {
         if ((is.hd(is.tl(seq)) - is.hd(seq)) < 0)
             if ((is.hd(is.tl(seq)) - is.hd(seq)) * -1 < eps) {
                 return is.hd(is.tl(seq));
             } else {
                 return sqrt(is.tl(seq), eps);
-        } else {
+            }
+        else {
             if ((is.hd(is.tl(seq)) - is.hd(seq)) < eps) {
                 return is.hd(is.tl(seq));
             } else {
@@ -45,12 +48,14 @@ module.exports = { };
     //                              Problem 3
     ///////////////////////////////////////////////////////////////////////////
 
-    var cumulate = function (seq) {
+    var cumulate = function(seq) {
         return is.cons(is.hd(seq), function() {
             return cumulate(is.cons(is.hd(seq) + is.hd(is.tl(seq)),
-            function() { return is.tl(is.tl(seq));}));
+                function() {
+                    return is.tl(is.tl(seq));
+                }));
         });
-        };
+    };
 
     // Do NOT modify this file below this point
 
@@ -58,4 +63,4 @@ module.exports = { };
     exports.sqrt = sqrt;
     exports.cumulate = cumulate;
 
-}) (module.exports);
+})(module.exports);
